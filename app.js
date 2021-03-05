c456c6bbecd30a4617fce60b59abd6c1b24ebece
Nail Khazeev <00008869@wiut.uz>
2021-03-05 16:53:22 +0500

    Created Pug files

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = require()
+
+app.get('/', (req, res) => ){
+
+}
+
+app.set('view engine'. 'pug')
+
+app.listen(8000, err =>{
+	if (err) throw err
+
+	console.log("App is running ....")
+})
